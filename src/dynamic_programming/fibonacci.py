31b33835fd249ddc701a486f3fba55cbd583cf9d
safecorners <safecorners.mobile@gmail.com>
2025-03-08 01:35:43 +0900

    test: test development configuration
    add fibonacci algorithm to validate type checker, linter, test runner

---
+++ src/dynamic_programming/fibonacci.py
@@ -0,0 +1,26 @@
+from typing import Callable, Concatenate, Dict, ParamSpec, TypeVar
+
+P = ParamSpec("P")
+R = TypeVar("R")
+T = TypeVar("T")
+
+
+def memorize(
+    func: Callable[Concatenate[T, P], R],
+) -> Callable[Concatenate[T, P], R]:
+    __cache: Dict[T, R] = {}
+
+    def _wrapper(n: T, *args: P.args, **kwargs: P.kwargs) -> R:
+        if n in __cache:
+            return __cache[n]
+        else:
+            result = func(n, *args, **kwargs)
+            __cache.update({n: result})
+            return result
+
+    return _wrapper
+
+
+@memorize
+def fib(n: int) -> int:
+    return n if n <= 1 else fib(n - 1) + fib(n - 2)
