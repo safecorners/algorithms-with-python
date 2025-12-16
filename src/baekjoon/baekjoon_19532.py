d59882665e918b9911fad94e170ea33170550831
safecorners <safecorners.mobile@gmail.com>
2025-12-17 04:48:56 +0900

    백준 19532

--- src/baekjoon/baekjoon_19532.py
+++ src/baekjoon/baekjoon_19532.py
@@ -1,94 +1,16 @@
-type Matrix = list[list[int]]
-type Vector = list[int]
+def find_solutions(numbers: list[int]) -> list[int]:
+    a, b, c, d, e, f = numbers
+    for x in range(-999, 999 + 1):
+        for y in range(-999, 999 + 1):
+            if a * x + b * y == c and d * x + e * y == f:
+                return [x, y]
 
-
-def gcd(a: int, b: int) -> int:
-    while True:
-        r = a % b
-        if r == 0:
-            return b
-        else:
-            a = b
-            b = r
-
-
-def lcm(a: int, b: int) -> int:
-    return (a * b) // gcd(a, b)
-
-
-def process(matrix: Matrix) -> Vector:
-    """
-    [1 0 x]
-    [0 1 y]
-    """
-    row_1 = matrix[0]
-    row_2 = matrix[1]
-
-    if row_1[0] == 0:
-        row_1, row_2 = row_2, row_1
-    # [1 0 x]
-    m = abs(lcm(row_1[1], row_2[1]))
-    if row_1[1] == m:
-        row_1 = [e for e in row_1]
-    else:
-        row_1 = [e * (m // row_1[1]) for e in row_1]
-
-    if row_2[1] == m:
-        row_2 = [e for e in row_2]
-    else:
-        row_2 = [e * (m // row_2[1]) for e in row_2]
-
-    # row_1 * -1
-    row_1 = [e * -1 for e in row_1]
-
-    row_1 = [row_1[i] + row_2[i] for i in range(3)]
-
-    m = abs(gcd(row_1[0], row_1[2]))
-
-    row_1 = [e // m for e in row_1]
-
-    if row_1[0] < 0:
-        row_1 = [e * -1 for e in row_1]
-
-    x = row_1[2]
-
-    # [0 1 y]
-    if row_2[1] == 0:
-        row_1, row_2 = row_2, row_1
-
-    m = abs(lcm(row_1[0], row_2[0]))
-
-    if row_1[0] == m:
-        row_1 = [e for e in row_1]
-    else:
-        row_1 = [e * (m // row_1[0]) for e in row_1]
-
-    if row_2[0] == m:
-        row_2 = [e for e in row_2]
-    else:
-        row_2 = [e * (m // row_2[0]) for e in row_2]
-
-    # row_2 * -1
-    row_2 = [e * -1 for e in row_2]
-
-    row_2 = [row_2[i] + row_1[i] for i in range(3)]
-
-    m = abs(gcd(row_2[1], row_2[2]))
-
-    row_2 = [e // m for e in row_2]
-
-    if row_2[1] < 0:
-        row_2 = [e * -1 for e in row_2]
-
-    y = row_2[2]
-    return [x, y]
+    raise ValueError
 
 
 if __name__ == "__main__":
     numbers = list(map(int, input().split(" ")))
 
-    matrix = [numbers[:3], numbers[3:]]
+    results = find_solutions(numbers)
 
-    vector = process(matrix)
-
-    print(f"{vector[0]} {vector[1]}")
+    print(*results)
--- tests/baekjoon/test_baekjoon_19532.py
+++ tests/baekjoon/test_baekjoon_19532.py
@@ -1,23 +1,13 @@
-from baekjoon.baekjoon_19532 import gcd, lcm, process
+from baekjoon.baekjoon_19532 import find_solutions
 
 
-def test_gcd() -> None:
-    assert gcd(24, 18) == 6
-
-
-def test_lcm() -> None:
-    assert lcm(24, 18) == 72
-
-    assert lcm(3, 1) == 3
-
-
-def test_case_one() -> None:
-    actual = process([[1, 3, -1], [4, 1, 7]])
+def test_case_1() -> None:
+    actual = find_solutions([1, 3, -1, 4, 1, 7])
 
     assert actual == [2, -1]
 
 
-def test_case_two() -> None:
-    actual = process([[2, 5, 8], [3, -4, -11]])
+def test_case_2() -> None:
+    actual = find_solutions([2, 5, 8, 3, -4, -11])
 
     assert actual == [-1, 2]
